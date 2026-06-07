19d13dc7f60c9cba3400ec75340502dc771de990
ThienNguyen2302 <nguyenngocthien749@gmail.com>
2026-06-07 21:02:12 +0700

    feat: add functionality to create and query a ChromaDB collection from tokenized PDF chunks

--- expansion_with_answers/expansion_answers.py
+++ expansion_with_answers/expansion_answers.py
@@ -21,8 +21,8 @@ texts = [text for text in pdf_text if text]
 character_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", ". ", " ", ""], chunk_size=1000, chunk_overlap=200)
 chunks = character_splitter.split_text("\n\n".join(texts))
 
-print(f"Number of chunks: {len(chunks)}")
-print(f"First chunk: {chunks[0]}")
+# print(f"Number of chunks: {len(chunks)}")
+# print(f"First chunk: {chunks[0]}")
 
 #tokenzing the chunks using sentence transformers tokenizer
 token_splitter = SentenceTransformersTokenTextSplitter(chunk_overlap=0, tokens_per_chunk=256)
@@ -31,8 +31,21 @@ token = []
 for chunks in chunks:
     token += token_splitter.split_text(chunks)
 
-print(f"Number of tokenized chunks: {len(token)}")
-print(f"First tokenized chunk: {token[0]}")
+# print(f"Number of tokenized chunks: {len(token)}")
+# print(f"First tokenized chunk: {token[0]}")
 
 embedding_function = SentenceTransformerEmbeddingFunction()
-print(embedding_function([token[0]]))
+# print(embedding_function([token[0]]))
+
+chromaCLient = chromadb.Client()
+collection = chromaCLient.create_collection(name="last_lecture_collection", embedding_function=embedding_function)
+
+ids = [str(i) for i in range(len(token))]
+collection.add(ids=ids, documents=token)
+count = collection.count()
+print(f"Number of documents in the collection: {count}")
+
+query = input("Enter your question: ")
+results = collection.query(query_texts=[query], n_results=5)
+retrieved_chunks = results['documents'][0]
+
